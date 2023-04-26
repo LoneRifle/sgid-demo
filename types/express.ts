f8931684db133cd7a2a220596d2f5355609607cd
LoneRifle <LoneRifle@users.noreply.github.com>
2023-04-26 10:24:44 +0800

    refactor(types): use SgidClient.prototype for type declaration

--- types/express.ts
+++ types/express.ts
@@ -1,14 +1,6 @@
-import { SgidClient } from '@opengovsg/sgid-client'
+import type { SgidClient } from '@opengovsg/sgid-client'
 
-// Just to generate the type
-const client = new SgidClient({
-  clientId: process.env.SGID_CLIENT_ID ?? '',
-  clientSecret: process.env.SGID_CLIENT_SECRET ?? '',
-  privateKey: process.env.SGID_PRIVATE_KEY ?? '',
-  redirectUri: process.env.SGID_REDIRECT_URI,
-})
-
-type UserInfo = Awaited<ReturnType<typeof client.userinfo>>
+type UserInfo = Awaited<ReturnType<typeof SgidClient.prototype.userinfo>>
 
 declare module 'express-session' {
   interface SessionData {
